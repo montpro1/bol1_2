be5252b4446bc7ab896069aa4772902a79e10689
Víctor Enrique Montes Tovar <vmontestovar@nautilus19.ad.danielcastelao.org>
2019-10-08 09:03:25 +0200

    primero

---
+++ bol1_2/Bol1_2.java
@@ -0,0 +1,17 @@
+
+package bol1_2;
+
+
+
+public class Bol1_2 {
+
+ 
+     
+    public static void main(String[] args) {
+        int lado=3;
+        int area=lado*lado;
+        System.out.println("el area del cuadrado es "+area);
+        
+    }
+    
+}
